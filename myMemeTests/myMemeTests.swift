d05347086a432ca2fa57a569e0c2011d3f0d2bfb
pamela032709 <iDevelopers@pamelas-imac.local>
2016-07-19 00:34:01 -0400

    Initial Commit

---
+++ myMemeTests/myMemeTests.swift
@@ -0,0 +1,36 @@
+//
+//  myMemeTests.swift
+//  myMemeTests
+//
+//  Created by Pamela Rios on 7/19/16.
+//  Copyright © 2016 Pamela Rios. All rights reserved.
+//
+
+import XCTest
+@testable import myMeme
+
+class myMemeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
