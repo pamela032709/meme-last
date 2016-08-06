d122664b6072693a2f63f694bde15188e675e509
pamela032709 <iDevelopers@Pamelas-iMac.local>
2016-08-05 22:59:08 -0400

    first commit

---
+++ myMeme/Meme3.swift
@@ -0,0 +1,18 @@
+//
+//  Meme3.swift
+//  myMeme
+//
+//  Created by Pamela Rios on 8/5/16.
+//  Copyright © 2016 Pamela Rios. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Meme{
+    var topField:String?
+    var bottomField:String?
+    var image:UIImage?
+    var memedImage:UIImage?
+    }
